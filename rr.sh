ed41cf824ff93ed8e73b3e612ca56d32fcade2b3
j4r e4o <j4r.e4o@gmail.com>
2010-12-26 14:49:21 -0500

    Eliminar archivos asociados a sqlite

--- rr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-rm initial_data.json
-python manage.py dumpdata > initial_data.json
-python manage.py reset academico
-python manage.py reset financiero
-python manage.py loaddata initial_data.json
-python manage.py runserver
