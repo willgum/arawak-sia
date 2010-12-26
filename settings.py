7d599068fbfe2e9a6a10fd632913a1455f3c0de5
j4r e4o <j4r.e4o@gmail.com>
2010-12-26 14:48:19 -0500

    Integracion MySQL

--- settings.py
+++ settings.py
@@ -12,14 +12,24 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+#'default': {
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': os.path.join(PROJECT_DIR, 'sia.db'), # Or path to database file if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_DIR, 'sia.db'), # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'arawakte_sia', 
+        'USER': 'arawakte_sia',
+        'PASSWORD': 'nTzIp;r,m$pP',
+        'HOST': 'arawaktechnologies.com',
+        'PORT': '3306',
     }
 }
 
