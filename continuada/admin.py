4d89f7973e8b2f5c500f2fd6fc6391b8eedd814e
j4r e4o <j4r.e4o@gmail.com>
2011-01-03 15:15:03 -0500

    Separar academico de edu. continuada.

--- continuada/admin.py
+++ continuada/admin.py
@@ -1,7 +1,85 @@
 # -*- coding: utf-8 -*-
-#from continuada.models import *
+from continuada.models import Curso, Estudiante, InscripcionCurso
 from django.contrib import admin
 
 
+class InscripcionCursoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': [
+            'fecha_inscripcion',
+            'curso', 
+            'estudiante',
+            'codigo',
+            'fecha_vencimiento', 
+            'nota']}),
+    ]
+    
+    list_display = (
+        'curso', 
+        'estudiante', 
+        'codigo',
+        'fecha_inscripcion',
+        'fecha_vencimiento',
+        'nota',
+    )
+    search_fields = ['curso', 'estudiante']
 
-#admin.site.register(***, ***Admin)
+class CursoAdmin(admin.ModelAdmin):
+    fieldsets = [
+         (None, {'fields': [ 
+            'tipo_curso',
+            'codigo', 
+            'nombre', 
+            'descripcion', 
+            'intensidad', 
+            'horario']}
+         ),
+    ]
+    list_display = (
+        'tipo_curso',
+        'codigo', 
+        'nombre', 
+        'intensidad',
+    )
+    
+    list_filter = ['tipo_curso',]
+    search_fields = ('nombre',)
+
+
+
+class InscripcionCursoInline(admin.TabularInline):
+    model = InscripcionCurso
+    extra = 1
+
+class EstudianteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Informacion Personal', {'fields': [ 
+            'nombre', 
+            'apellido', 
+            'sexo', 
+            'tipo_documento', 
+            'documento', 
+            'lugar_expedicion',
+            'fecha_nacimiento', 
+            'lugar_nacimiento'], 
+            'classes': ['collapse']}),
+        ('Informacion de contacto', {'fields': [
+            'direccion', 
+            'lugar_residencia', 
+            'telefono', 
+            'email', 
+            'web'], 
+            'classes': ['collapse']}),
+        ('Informacion de acceso', {'fields': [
+            'usuario', 
+            'contrasena'], 
+            'classes': ['collapse']}),
+    ]
+    inlines = [InscripcionCursoInline]
+    list_display = ('documento', 'nombre', 'apellido')
+    search_fields = ['documento', 'nombre', 'apellido']
+
+
+admin.site.register(Estudiante, EstudianteAdmin)
+admin.site.register(Curso, CursoAdmin)
+admin.site.register(InscripcionCurso, InscripcionCursoAdmin)
--- continuada/models.py
+++ continuada/models.py
@@ -1,4 +1,63 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-#TODO: Separar los cursos de educacion continuada definidos en el modulos academico
+TIPO_CURSO = (
+    ('D', 'Diplomado'),
+    ('S', 'Seminario'),
+    ('I', 'Intensivo'),
+    ('P', 'Personalizado'),
+)
+
+TIPO_DOCUMENTO = (
+    ('C', 'Cédula de ciudadania'),
+    ('T', 'Tarjeta de identidad'),
+    ('P', 'Pasaporte'),
+)
+
+SEXO = (
+    ('M', 'Masculino'),
+    ('F', 'Femenino'),
+)
+
+class Estudiante(models.Model):
+    # Informacion personal  
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    sexo = models.CharField(max_length=1, choices=SEXO, blank=True) 
+    tipo_documento = models.CharField(max_length=1, choices=TIPO_DOCUMENTO, blank=True) 
+    documento = models.CharField(max_length=200, unique=True)
+    lugar_expedicion = models.CharField(max_length=200, verbose_name='Lugar expedición', blank=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    lugar_nacimiento = models.CharField(max_length=200, blank=True)
+    # Informacion de contacto
+    direccion = models.CharField(verbose_name='Dirección', max_length=200, blank=True)
+    lugar_residencia = models.CharField(max_length=200, blank=True)
+    telefono = models.CharField(verbose_name='Teléfono', max_length=200, blank=True)
+    email = models.EmailField(unique=True, blank=True)
+    web = models.URLField(blank=True)
+    # Informacion de acceso
+    usuario = models.CharField(max_length=200, unique=True, blank=True)
+    contrasena = models.CharField(verbose_name='Contraseña', max_length=200, blank=True)
+    
+    def __unicode__(self):
+        return self.documento
+
+class Curso(models.Model):
+    tipo_curso = models.CharField(max_length=1, choices=TIPO_CURSO, blank=True)
+    codigo = models.CharField(verbose_name='Código', max_length=200)
+    nombre = models.CharField(max_length=200, blank=True)
+    descripcion = models.TextField(verbose_name='Descripción', max_length=200, blank=True)
+    intensidad = models.IntegerField(help_text='Número de horas.', blank=True, null=True)
+    horario = models.CharField(max_length=200, blank=True)
+    
+    def __unicode__(self):
+        return self.codigo
+    
+class InscripcionCurso(models.Model):
+    fecha_inscripcion = models.DateField()
+    estudiante = models.ForeignKey(Estudiante)
+    curso = models.ForeignKey(Curso)
+    codigo = models.CharField(verbose_name='Código', max_length=200)
+    fecha_vencimiento = models.DateField(blank=True, null=True)
+    nota = models.FloatField(blank=True, null=True)
+    
