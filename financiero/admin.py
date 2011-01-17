028a1227e1fe217061c9991d0e5908f74bd17ef9
j4r e4o <j4r.e4o@gmail.com>
2011-01-17 17:35:18 -0500

    Agregar programas a los datos iniciales de Poliagro

---
+++ financiero/admin.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from financiero.models import Pago, Letra, MatriculaFinanciera, Multa, HoraCatedra
+from django.contrib import admin
+
+
+class PagoInline(admin.TabularInline):
+    model = Pago
+    extra = 1
+  
+  
+class LetraInline(admin.TabularInline):
+    model = Letra
+    extra = 1
+  
+  
+class MatriculaFinancieraAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': [    'fecha_expedicion',
+                                'inscripcion_estudiante', 
+                                'estado',
+                                'becado',
+                                'valor_inscripcion',
+                                'valor_matricula',
+                                'cuotas',
+                                'cancelada']}),
+    ]
+    inlines = [PagoInline, LetraInline]
+  
+  
+class MultaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': [  'fecha_expedicion',
+                              'matricula_financiera',
+                              'valor',
+                              'concepto',
+                              'fecha_pago',
+                              'cancelada']}),
+        ]
+  
+  
+class HoraCatedraAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': [    'profesor',
+                                'curso',
+                                'valor_hora',
+                                'observaciones']}),
+    ]
+
+
+admin.site.register(HoraCatedra, HoraCatedraAdmin)
+admin.site.register(MatriculaFinanciera, MatriculaFinancieraAdmin)
+admin.site.register(Multa, MultaAdmin)
