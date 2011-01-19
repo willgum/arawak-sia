7c376cfae691dc853f33795d84fb2b010506aeb7
j4r e4o <j4r.e4o@gmail.com>
2011-01-18 19:22:03 -0500

    Merge with c571e77febe2d88593c388641f16bc2966c115dc

--- academico/models.py
+++ academico/models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os.path
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -176,6 +175,9 @@ class Profesor(models.Model):
 #    Grupo 3 corresponde en la base de datos con un perfil profesor
 
     def save(self, *args, **kwargs):
+        if self.foto.name != "":
+            self.foto.name = self.documento + "c.jpg"
+        
         try:
                 user = User.objects.get(username=self.documento)
         except User.DoesNotExist:
@@ -253,9 +255,9 @@ class Estudiante(models.Model):
     lugar_nacimiento = models.CharField(max_length=200, blank=True)
     
     # Requisitos
-    fotocopia_documento = models.FileField(upload_to='imagenes/original/', blank=True)
-    fotocopia_diploma = models.FileField(upload_to='imagenes/original/', blank=True)
-    foto = models.FileField(upload_to='imagenes/original/', blank=True)
+    fotocopia_documento = models.ImageField(upload_to='imagenes/original/', blank=True)
+    fotocopia_diploma = models.ImageField(upload_to='imagenes/original/', blank=True)
+    foto = models.ImageField(upload_to='imagenes/original/', blank=True)
     
     # Informacion de contacto
     direccion = models.CharField( verbose_name='Dirección', max_length=200, blank=True)
