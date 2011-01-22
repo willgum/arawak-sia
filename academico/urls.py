28b1b18b1e2f5e91a32ae49c4449fa7c346bb310
rogerleonardoh <rogerleonardoh@gmail.com>
2011-01-22 09:13:26 -0500

    terminado programas y parte de competencias

--- academico/urls.py
+++ academico/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls.defaults import *
 from academico.views import *
 
 urlpatterns = patterns('',
-    # contrasena y actualizar contrasena
-    (r'^programas/$', programas),  
+    # ver programas a los que un docente o profesor se encuetra vinculado
+    (r'^programas/$', programas),
+    (r'^programas/(?P<programa_id>\d+)/$', programasDetalle), 
+    
+    (r'^competencias/$', competencias), 
+    (r'^competencias/(?P<competencia_id>\d+)/$', competenciasDetalle),
 )
--- academico/views.py
+++ academico/views.py
@@ -5,7 +5,7 @@ from django.views.static import *                               # se incorporo p
 from django.conf import settings                                # se incopora para poder acceder a los valores creados en el settings
 from django.contrib import auth                                   
 from django.contrib.auth.models import Group
-from academico.models import Profesor, Estudiante, Curso, Competencia, Programa, MatriculaPrograma
+from academico.models import Profesor, Estudiante, Curso, Competencia, Programa, MatriculaPrograma, Periodicidad
 from django.contrib.auth.decorators import login_required                           # me permite usar eö @login_requerid
 
 @login_required
@@ -56,4 +56,108 @@ def programas(request):
                 'path': settings.MEDIA_URL,
                 'msg_error': 'Lo sentimos, al parecer no esta inscrito a ningun programa.',
             }) 
+    return render_to_response(plantilla, variables, context_instance=RequestContext(request))
+
+@login_required
+def programasDetalle(request, programa_id):
+    plantilla = "academico/programaDetalle.html",
+    grupos = request.user.groups.all()    
+    for grupo in grupos:
+        grupoUsuario = Group.objects.get(name = grupo)
+    try:
+        grupoUsuario
+    except:
+        variables = Context({
+            'user': request.user, 
+            'titulo': '.: SIA - Sistema de Información Académica :.',
+            'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+            'path': settings.MEDIA_URL,
+            'msg_error': 'Lo sentimos, no puede acceder a esta informacion.',
+        })
+    else:
+        programa = Programa.objects.get(id = programa_id)
+        variables = Context({
+            'user': request.user, 
+            'titulo': '.: SIA - Sistema de Información Académica :.',
+            'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+            'path': settings.MEDIA_URL,
+            'programa': programa,
+            
+        })  
+    return render_to_response(plantilla, variables, context_instance=RequestContext(request))
+
+@login_required
+def competencias(request):
+    plantilla = "academico/competencias.html",
+    grupos = request.user.groups.all()    
+    for grupo in grupos:
+        grupoUsuario = Group.objects.get(name = grupo)
+    try:
+        grupoUsuario
+    except:
+        variables = Context({
+            'user': request.user, 
+            'titulo': '.: SIA - Sistema de Información Académica :.',
+            'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+            'path': settings.MEDIA_URL,
+            'msg_error': 'Lo sentimos, no tiene competencias inscritas.',
+        })
+    else:
+        if grupoUsuario.id == 3 or grupoUsuario.id == 4:
+            if grupoUsuario.id == 3:
+                usuario = Profesor.objects.get(documento = request.user)
+                competencias = Competencia.objects.filter(curso__profesor = usuario.id).distinct()
+                variables = Context({
+                'user': request.user, 
+                'titulo': '.: SIA - Sistema de Información Académica :.',
+                'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+                'path': settings.MEDIA_URL,
+                'competencias': competencias,
+                'cantidad': len(competencias)
+                })
+            else:
+                usuario = Estudiante.objects.get(documento = request.user)
+                programas = Programa.objects.filter(id = MatriculaPrograma.objects.filter(estudiante = usuario.id)).distinct()
+                variables = Context({
+                'user': request.user, 
+                'titulo': '.: SIA - Sistema de Información Académica :.',
+                'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+                'path': settings.MEDIA_URL,
+                'programas': programas,
+                'cantidad': len(programas)
+                })
+        else:
+            variables = Context({
+                'user': request.user, 
+                'titulo': '.: SIA - Sistema de Información Académica :.',
+                'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+                'path': settings.MEDIA_URL,
+                'msg_error': 'Lo sentimos, no tiene competencias inscritas.',
+            }) 
+    return render_to_response(plantilla, variables, context_instance=RequestContext(request))
+@login_required
+def competenciasDetalle(request, competencia_id):
+    plantilla = "academico/competenciaDetalle.html",
+    grupos = request.user.groups.all()    
+    for grupo in grupos:
+        grupoUsuario = Group.objects.get(name = grupo)
+    try:
+        grupoUsuario
+    except:
+        variables = Context({
+            'user': request.user, 
+            'titulo': '.: SIA - Sistema de Información Académica :.',
+            'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+            'path': settings.MEDIA_URL,
+            'msg_error': 'Lo sentimos, no puede acceder a esta informacion.',
+        })
+    else:
+        competencia = Competencia.objects.get(id = competencia_id)
+        variables = Context({
+            'user': request.user, 
+            'titulo': '.: SIA - Sistema de Información Académica :.',
+            'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
+            'path': settings.MEDIA_URL,
+            'competencia': competencia
+        })  
     return render_to_response(plantilla, variables, context_instance=RequestContext(request))
