531fc6eb40d4974a5a8e84f0c16bf9d5e7b66c10
rogerleonardoh <rogerleonardoh@gmail.com>
2011-01-24 11:54:01 -0500

    se agregaron datos a initial_data para los diferentes tipos de usuarios, se modifico models para poder traer desde calficaciones la competencia y el programa al que se encuetra vinculado un usuario, por ultimo se completaron las vistas de competencias por docente y estudiante

--- academico/views.py
+++ academico/views.py
@@ -5,7 +5,7 @@ from django.views.static import *                               # se incorporo p
 from django.conf import settings                                # se incopora para poder acceder a los valores creados en el settings
 from django.contrib import auth                                   
 from django.contrib.auth.models import Group
-from academico.models import Profesor, Estudiante, Curso, Competencia, Programa, MatriculaPrograma, Periodicidad
+from academico.models import Profesor, Estudiante, Curso, Competencia, Programa, MatriculaPrograma, MatriculaCiclo, Periodicidad, Calificacion
 from django.contrib.auth.decorators import login_required                           # me permite usar eö @login_requerid
 
 @login_required
@@ -28,25 +28,53 @@ def programas(request):
         if grupoUsuario.id == 3 or grupoUsuario.id == 4:
             if grupoUsuario.id == 3:
                 usuario = Profesor.objects.get(documento = request.user)
-                programas = Programa.objects.filter(id = Competencia.objects.filter(curso__profesor = usuario.id)).distinct()
+                competencias = Competencia.objects.filter(curso__profesor = usuario.id).distinct()
+                
+                programas = []
+                for competencia in competencias:
+                    programas.append(Programa.objects.get(id = competencia.programa_id))
+                
+                listaProgramas = []
+                for indice1 in range(0, len(programas)):
+                    contador = 0
+                    for indice2 in range(indice1, len(programas)):
+                        if programas[indice1].codigo == programas[indice2].codigo:
+                            contador += 1
+                    if contador <= 1:
+                        listaProgramas.append(programas[indice1]);                    
+                    
                 variables = Context({
                 'user': request.user, 
                 'titulo': '.: SIA - Sistema de Información Académica :.',
                 'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
                 'path': settings.MEDIA_URL,
-                'programas': programas,
-                'cantidad': len(programas)
+                'programas': listaProgramas,
+                'cantidad': len(listaProgramas)
                 })
             else:
                 usuario = Estudiante.objects.get(documento = request.user)
-                programas = Programa.objects.filter(id = MatriculaPrograma.objects.filter(estudiante = usuario.id)).distinct()
+                matriculas = MatriculaPrograma.objects.filter(estudiante = usuario.id)
+                
+                programas = []
+                for matricula in matriculas:
+                    programas.append(Programa.objects.get(id = matricula.programa_id))
+                
+                listaProgramas = []
+                for indice1 in range(0, len(programas)):
+                    contador = 0
+                    for indice2 in range(indice1, len(programas)):
+                        if programas[indice1].codigo == programas[indice2].codigo:
+                            contador += 1
+                    if contador <= 1:
+                        listaProgramas.append(programas[indice1]);
+                        
                 variables = Context({
                 'user': request.user, 
                 'titulo': '.: SIA - Sistema de Información Académica :.',
                 'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
                 'path': settings.MEDIA_URL,
-                'programas': programas,
-                'cantidad': len(programas)
+                'programas': listaProgramas,
+                'cantidad': len(listaProgramas)
                 })
         else:
             variables = Context({
@@ -113,18 +141,43 @@ def competencias(request):
                 'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
                 'path': settings.MEDIA_URL,
                 'competencias': competencias,
-                'cantidad': len(competencias)
+                'cantidad': len(competencias),
+                'grupoUsuario': grupoUsuario.id,
                 })
             else:
                 usuario = Estudiante.objects.get(documento = request.user)
-                programas = Programa.objects.filter(id = MatriculaPrograma.objects.filter(estudiante = usuario.id)).distinct()
+                matPrograma = MatriculaPrograma.objects.filter(estudiante = usuario.id)
+                
+                matCiclo = []
+                for mat in matPrograma:
+                    resultado = MatriculaCiclo.objects.filter(matricula_programa = mat.id)
+                    if len(resultado) > 0:
+                        for indice1 in range(0, len(resultado)):
+                            contador = 0
+                            if len(matCiclo) > 0:
+                                for indice2 in range(0, len(matCiclo)):
+                                    if resultado[indice1] == matCiclo[indice2]:
+                                        contador += 1
+                                    if contador == 0:
+                                        matCiclo.append(resultado[indice1])
+                            else:
+                                matCiclo.append(resultado[indice1])
+                
+                calificaciones = []
+                for mat in matCiclo:
+                    resultado = Calificacion.objects.filter(matricula_ciclo = mat.id)                    
+                    if len(resultado) > 0:
+                        for indice in range(0, len(resultado)):
+                            calificaciones.append(resultado[indice])
+                                
                 variables = Context({
                 'user': request.user, 
                 'titulo': '.: SIA - Sistema de Información Académica :.',
                 'titulo_pagina': '.: SIA - Sistema de Información Académica :.',
                 'path': settings.MEDIA_URL,
-                'programas': programas,
-                'cantidad': len(programas)
+                'calificaciones': calificaciones,
+                'grupoUsuario': grupoUsuario.id,
+                'cantidad': len(calificaciones)
                 })
         else:
             variables = Context({
@@ -135,6 +188,7 @@ def competencias(request):
                 'msg_error': 'Lo sentimos, no tiene competencias inscritas.',
             }) 
     return render_to_response(plantilla, variables, context_instance=RequestContext(request))
+
 @login_required
 def competenciasDetalle(request, competencia_id):
     plantilla = "academico/competenciaDetalle.html",
