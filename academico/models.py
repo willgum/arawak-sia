e0f76020af5f02d8d833630d821ff04ddda9697b
edwarcarranza <edwarcarranza@localhost>
2011-01-26 14:24:50 -0500

    Se modific? archivo poliagro.json para agregar registros a grupo, y se corrigi? consulta sql en m?todo guardar de la clase estudiante.

--- academico/models.py
+++ academico/models.py
@@ -400,7 +400,7 @@ class MatriculaPrograma(models.Model):
             self.codigo = tmp_codigo
         else:
             tmp_codigo = tmp_codigo[0:4]
-            for c in MatriculaPrograma.objects.raw('SELECT id, lpad(cast(right(codigo, 4) as signed)+1, 4, 0) AS codigo FROM academico_MatriculaPrograma where left(codigo, 4) = %s limit 0, 1', [tmp_codigo]):
+            for c in MatriculaPrograma.objects.raw('SELECT id, lpad(cast(right(codigo, 4) as signed)+1, 4, 0) AS codigo FROM academico_matriculaprograma where left(codigo, 4) = %s limit 0, 1', [tmp_codigo]):
                 self.codigo = "%s%s" %(tmp_codigo, c.codigo)
             
         super(MatriculaPrograma, self).save(*args, **kwargs)
