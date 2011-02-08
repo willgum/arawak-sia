cf40eca095bfe66ab0295c7c993e9388ef1bf72e
rogerleonardoh <rogerleonardoh@gmail.com>
2011-02-08 11:02:42 -0500

    modificacion en plantillas para controlar el acceso a sonas restringidas

--- academico/views.py
+++ academico/views.py
@@ -53,7 +53,7 @@ def logout(solicitud):
     if 'msg_error' in solicitud.session:
         solicitud.session['msg_error']
     auth.logout(solicitud)    
-    return HttpResponseRedirect("/")
+    return HttpResponseRedirect(settings.PROJECT_DIR)
 
 #----------------------------------------------vistas docente---------------------------------------------------------
 
