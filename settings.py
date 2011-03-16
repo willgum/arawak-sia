6d020a2844306c19ca70cba328184e6e5236165a
rogerleonardoh <rogerleonardoh@localhost>
2011-03-16 16:11:27 -0500

    prueba

--- settings.py
+++ settings.py
@@ -31,7 +31,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'syspro', 
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
